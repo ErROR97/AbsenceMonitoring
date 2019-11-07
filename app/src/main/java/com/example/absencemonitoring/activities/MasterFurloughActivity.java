1b7a67f4c75a5ff64c71205d5233e8395e46a8d8
Martin <matingrimes@yahoo.com>
2019-11-07 13:36:43 +0330

    :\

--- app/src/main/java/com/example/absencemonitoring/activities/MasterFurloughActivity.java
+++ app/src/main/java/com/example/absencemonitoring/activities/MasterFurloughActivity.java
@@ -147,7 +147,7 @@ public class MasterFurloughActivity extends AppCompatActivity {
                 });
             }
         });
-        //,jljk
+        //,jljkبیسبسیبسیب
 
         sendRejectBtn.setOnClickListener(new View.OnClickListener() {
             @Override
